c4c7bf97b277857f5901decf0d4fd63d6ee4d5b6
Bruno Silva <brunocs86@outlook.com>
2018-09-10 23:32:56 -0300

    api rest atualizada com classes AbstractDAO

--- 06092018/src/LojaAgua/persistencia/AbstractDAO.php
+++ 06092018/src/LojaAgua/persistencia/AbstractDAO.php
@@ -1,72 +1,54 @@
 <?php
-/*/**
- * Created by PhpStorm.
- * User: bcs
- * Date: 10/09/2018
- * Time: 22:49
- */
 
 namespace LojaAgua\persistencia;
 
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Setup;
 
-abstract class AbstractDAO
-{
+abstract class AbstractDAO {
     public $entityManager;
     private $entityPath;
-
-    public function __construct($entityPath)
-    {
+    public function __construct($entityPath) {
         $this->entityPath = $entityPath;
-        $this->entityManager = $this->createEntityManager();
+        $this->entityManager = $this->createEntityManager ();
     }
-
-    public function createEntityManager(){
-        $path = array(
-            'Loja/entidades'
+    public function createEntityManager() {
+        $path = array (
+            'LojaAgua/entidades'
         );
-
         $devMode = true;
-
-        $config = Setup::createAnnotationMetadataConfiguration($path, $devMode);
-
-        $connectionOptions = array(
+        $config = Setup::createAnnotationMetadataConfiguration ( $path, $devMode );
+        $connectionOptions =  array (
             'dbname' => 'vendaagua',
             'user' => 'root',
             'password' => '12345678',
             'host' => 'localhost',
             'driver' => 'pdo_mysql'
         );
-
-        return EntityManager::create($connectionOptions, $config);
+        //Passo 3 - Objeto de persistencia
+        return EntityManager::create ( $connectionOptions, $config );
     }
-
-    public function insert($user){
-        $this->entityManager->persist($user);
+    public function insert($obj){
+        $this->entityManager->persist($obj);
         $this->entityManager->flush();
     }
-    public function update($user){
-        $this->entityManager->merge($user);
+    public function update($obj){
+        $this->entityManager->merge($obj);
         $this->entityManager->flush();
     }
-    public function delete($id){
-        $this->entityManager->remove($id);
+    public function delete($obj){
+        $this->entityManager->remove($obj);
         $this->entityManager->flush();
     }
     public function findById($id){
-
-        return $this->entityManager ->find('LojaAgua\entidades\Usuario' ,$id);
+        return $this->entityManager ->find ( $this->entityPath ,$id) ;
     }
     public function findAll(){
-        $collection = $this->entityManager->getRepository($this->entityPath)->findAll();
-
-        $data = array();
-        foreach ($collection as $obj){
+        $collection = $this->entityManager ->getRepository ( $this->entityPath )->findAll ();
+        $data = array ();
+        foreach ( $collection as $obj ) {
             $data [] = $obj;
         }
-
         return $data;
     }
-
 }
--- 06092018/src/LojaAgua/persistencia/UsuarioDAO.php
+++ 06092018/src/LojaAgua/persistencia/UsuarioDAO.php
@@ -1,22 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bcs
- * Date: 09/09/2018
- * Time: 21:21
- */
-
 namespace LojaAgua\persistencia;
 
-use  Doctrine\ORM\EntityManager;
-use  Doctrine\ORM\Tools\Setup;
-use  LojaAgua\persistencia\AbstractDAO;
-use  LojaAgua\entidades\Usuario;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
+use LojaAgua\persistencia\AbstractDAO;
+use LojaAgua\entidades\Usuario;
 
-class UsuarioDAO extends AbstractDAO
-{
-    public function __construct()
-    {
-        parent::__contruct('LojaAgua\entidades\Usuario');
+class UsuarioDAO extends AbstractDAO{
+    public function __construct() {
+        parent::__construct('LojaAgua\entidades\Usuario');
     }
 }
