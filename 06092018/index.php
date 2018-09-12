97f15e8f7fbb21cc22667f9dcdf5f95091a97c44
Bruno Silva <brunocs86@outlook.com>
2018-09-12 17:43:10 -0300

    verificando login

--- 06092018/index.php
+++ 06092018/index.php
@@ -7,7 +7,16 @@ use LojaAgua\entidades\Pedido;
 use LojaAgua\persistencia\UsuarioDAO;
 use LojaAgua\persistencia\PedidoDAO;
 
-$user = new Usuario(0, "rebeca@gmail.com", "123", "rua X");
+/*$user = new Usuario(0, "nice@gmail.com", "123", "rua X");
 $dao = new UsuarioDAO();
 
-$dao->insert($user);
+$dao->insert($user);*/
+
+// DEFINE O FUSO HORARIO COMO O HORARIO DE BRASILIA
+date_default_timezone_set('America/Sao_Paulo');
+// CRIA UMA VARIAVEL E ARMAZENA A HORA ATUAL DO FUSO-HORÀRIO DEFINIDO (BRASÍLIA)
+$time = new DateTime("now");
+$pedido = new Pedido(0, $time, null, array());
+$dao2 = new PedidoDAO($pedido);
+
+$dao2->insert($pedido);
